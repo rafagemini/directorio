af23ac5b7170d82f30c7c9fb278edd5435532ff8
Rafael <rafael@ubuntu.(none)>
2013-09-07 20:40:16 -0500

    cambios en el tpl de contacto

--- directorio/contacto-ver.tpl.php
+++ directorio/contacto-ver.tpl.php
@@ -1,1 +1,3 @@
-<p class="saludo"><em><?php echo $cid; ?></em></p>
+<?php dsm($contacto); ?>
+<p class="saludo"><em><?php print $contacto->nombre; ?></em></p>
+<p>Fecha de ultimo acceso : <?php print $ultimaconsulta ?></p>
